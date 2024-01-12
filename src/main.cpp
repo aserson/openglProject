eb7a5a094451a8f575438d823b553f5df3661d75
TimoSpaceNomad <zinyakov.timofey@gmail.com>
2024-01-12 10:46:27 +0400

    GL-11 Uniforms in OpenGL
    https://youtu.be/DE6Xlx_kbo0

--- src/main.cpp
+++ src/main.cpp
@@ -131,6 +131,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
@@ -164,14 +166,29 @@ int main(void)
     ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
+    
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != 1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
