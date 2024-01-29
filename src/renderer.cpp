c7e158518cc65f89954af243a69c04ad19473eb0
TimoSpaceNomad <zinyakov.timofey@gmail.com>
2024-01-29 07:37:36 +0400

    GL-008 Add classes implementation of GL elements

---
+++ src/renderer.cpp
@@ -0,0 +1,19 @@
+#include "renderer.h"
+
+#include <iostream>
+ 
+void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogError(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function <<
+            " " << file << ": " << line << std::endl;
+        return false;
+    }
+    return true;
+}
